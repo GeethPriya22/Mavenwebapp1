fc8820039e42dabec3af10f47eafe5f21f9697c5
GeethPriya22 <geethshiva98@gmail.com>
2020-12-21 20:19:54 +0530

    first commit

---
+++ src/main/java/com/lti/app/Servlet1.java
@@ -0,0 +1,20 @@
+package com.lti.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("<h1>Sevlet page</h1>");
+		out.println("<h2>Welcome "+name+"</h2>");
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="Servlet1" method="get">
+Name: <input type="text" name="name"><br>
+<button>Submit</button>
+</form>
+</body>
+</html>
